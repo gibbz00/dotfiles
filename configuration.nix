aae3687c90897e0d0818f8af0e39cae09d18a304
gibbz00 <gabrielhansson00@gmail.com>
2026-01-25 17:33:36 +0100

    feat: infer openssh firewall port opening

--- configuration.nix
+++ configuration.nix
@@ -1,20 +1,21 @@
 { pkgs, ... }:
+let
+  rootPassword = "test";
+  hostSshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAZLVfPatp7YOYiWAmpDMibN9CNLCmqEOhWZ8bsqvENa gibbz@evolve-leissner";
+in
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.firewall.allowedTCPPorts = [
-    22
-  ];
-
   users.users.root = {
-    initialPassword = "test";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAZLVfPatp7YOYiWAmpDMibN9CNLCmqEOhWZ8bsqvENa gibbz@evolve-leissner"
-    ];
+    initialPassword = rootPassword;
+    openssh.authorizedKeys.keys = [ hostSshKey ];
   };
 
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+  };
 
   environment.systemPackages = with pkgs; [
     vim
