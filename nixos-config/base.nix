1e15499f091cb4de1619dc0128724ccb545b1dde
gibbz00 <gabrielhansson00@gmail.com>
2026-02-01 15:06:47 +0100

    feat: nix store GC and size optimizaitons

--- nixos-config/base.nix
+++ nixos-config/base.nix
@@ -15,9 +15,17 @@
     };
   };
 
-  ## Nix settings and optimizations
+  ## Nix base settings
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
+
+  ### Nix store GC and size optimizations
+  nix.settings.auto-optimise-store = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
 }
