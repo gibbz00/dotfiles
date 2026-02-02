1d01992457243ed1b99f8784cdd4d9ea3ef9ca83
gibbz00 <gabrielhansson00@gmail.com>
2026-02-02 13:02:23 +0100

    feat: secure NTP

--- nixos-config/base.nix
+++ nixos-config/base.nix
@@ -7,6 +7,9 @@
     flake-inputs.home-manager.nixosModules.home-manager
   ];
 
+  # See: https://discourse.nixos.org/t/why-are-we-using-systemd-timesyncd-by-default/74052/22
+  services.ntpd-rs.enable = true;
+
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
