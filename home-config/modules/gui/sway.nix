499daaef068e2e47415487ac4af184d80fa27fc3
gibbz00 <gabrielhansson00@gmail.com>
2026-02-05 15:43:37 +0100

    feat(home/gui): sway keybindings

--- home-config/modules/gui/sway.nix
+++ home-config/modules/gui/sway.nix
@@ -2,11 +2,26 @@
 {
   home.packages = with pkgs; [
     upower
+    brightnessctl
   ];
 
   wayland.windowManager.sway =
     let
       mod = "Mod4";
+
+      ws_bind =
+        { k, v }:
+        let
+          inherit (pkgs.lib) toString forEach range;
+          workspaces = forEach (range 1 9) (n: toString n);
+        in
+        builtins.listToAttrs (
+          forEach workspaces (ws: {
+            name = k ws;
+            value = v ws;
+          })
+        );
+
     in
     {
       enable = true;
@@ -23,6 +38,7 @@
       '';
       config = {
         modifier = mod;
+        floating.modifier = mod;
 
         seat."*".hide_cursor = "when-typing enable";
 
@@ -55,6 +71,56 @@
             }
           )
         ];
+
+        keybindings =
+          ws_bind {
+            k = ws: "$mod+${ws}";
+            v = ws: "workspace ${ws}";
+          }
+          // ws_bind {
+            k = ws: "$mod+Shift+${ws}";
+            v = ws: "move container to workspace ${ws}";
+          }
+          // {
+            "$mod+q" = "kill";
+            "$mod+f" = "fullscreen toggle";
+            "$mod+space" = "floating toggle";
+            "$mod+Shift+z" = "move scratchpad";
+            "$mod+z" = "scratchpad show";
+
+            # Navigation
+            "$mod+bracketleft" = "workspace prev";
+            "$mod+bracketright" = "workspace next";
+            "$mod+Tab" = "workspace back_and_forth";
+            "$mod+Shift+Tab" = "move container to workspace back_and_forth; workspace back_and_forth";
+            # Layouts
+            "$mod+Shift+t" = "layout tabbed";
+            "$mod+Shift+s" = "layout stacking";
+            "$mod+Shift+v" = "layout toggle split";
+            "$mod+v" = "split toggle";
+            # Sizing
+            "$mod+Control+$down" = "resize shrink height 90 px";
+            "$mod+Control+$up" = "resize grow height 90 px";
+            "$mod+Control+$right" = "resize grow width  90 px";
+            "$mod+Control+$left" = "resize shrink width  90 px";
+            # Move float
+            "$mod+Shift+$left" = "move left 90px";
+            "$mod+Shift+$down" = "move down 90px";
+            "$mod+Shift+$up" = "move up 90px";
+            "$mod+Shift+$right" = "move right 90px";
+            # Change focus
+            "$mod+$left" = "focus left";
+            "$mod+$down" = "focus down";
+            "$mod+$up" = "focus up";
+            "$mod+$right" = "focus right";
+            "$mod+Shift+space" = "focus mode_toggle";
+
+            # Exec
+            "$mod+Return" = "exec footclient";
+
+            "XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
+            "XF86MonBrightnessUp" = "exec brightnessctl set 5%+";
+          };
       };
     };
 
