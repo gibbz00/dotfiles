f74392ff6ed735a5b7a76813a45daa2706419343
gibbz00 <gabrielhansson00@gmail.com>
2026-02-05 18:51:49 +0100

    feat(home/gui): rofi exit launcher

--- home-config/modules/gui/rofi.nix
+++ home-config/modules/gui/rofi.nix
@@ -2,6 +2,24 @@
 {
   wayland.windowManager.sway.config.keybindings = {
     "$mod+o" = "exec rofi -show drun -show-icons";
+    "$mod+d" = "exec --no-startup-id \"$XDG_CONFIG_HOME/rofi/exit.sh\"";
+  };
+
+  xdg.configFile."rofi/exit.sh" = {
+    executable = true;
+    text = ''
+      #!/bin/sh
+      ANS="$(echo "Lock|Logout|Reboot|Shutdown" \
+        | rofi -dmenu -sep "|" -p 'System Exit' -theme-str 'listview { scrollbar: false; }'
+      )"
+
+      case "$ANS" in
+        *Lock) swaylock ;;
+        *Logout*) swaymsg exit ;;
+        *Reboot) systemctl reboot ;;
+        *Shutdown) systemctl -i poweroff ;;
+      esac
+    '';
   };
 
   programs.rofi = {
