2fc6dfb00ab624fc0b9b6135d470ff6e73effec5
gibbz00 <gabrielhansson00@gmail.com>
2026-02-07 22:22:04 +0100

    fix(home/gui): fnott action selection with rofi

--- home-config/modules/gui/sway/fnott.nix
+++ home-config/modules/gui/sway/fnott.nix
@@ -1,8 +1,16 @@
 { ... }:
 {
-  services.fnott.enable = true;
+  services.fnott = {
+    enable = true;
+    settings = {
+      main = {
+        selection-helper = "rofi -sep '\0' -dmenu";
+        selection-helper-uses-null-separator = true;
+      };
+    };
+  };
   wayland.windowManager.sway.config.keybindings = {
     "$mod+BackSpace" = "exec fnottctl dismiss";
-    "$mod+Colon" = "exec context actions";
+    "$mod+Colon" = "exec fnottctl actions";
   };
 }
