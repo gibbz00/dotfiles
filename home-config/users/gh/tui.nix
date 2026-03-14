0bd3cefa3ea5932ea2568b26a4b4b02c4b7952b6
gibbz00 <gabrielhansson00@gmail.com>
2026-03-14 18:54:02 +0100

    feat(sys): add docker proxy settings

--- home-config/users/gh/tui.nix
+++ home-config/users/gh/tui.nix
@@ -47,7 +47,6 @@
   };
 
   home.packages = with pkgs; [
-    docker-compose
     openconnect
     vpn-slice
 
--- nixos-config/modules/docker.nix
+++ nixos-config/modules/docker.nix
@@ -1,5 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  # TEMP:
+  systemd.services.docker.environment = {
+    HTTP_PROXY = "http://192.168.1.1:80";
+    HTTPS_PROXY = "http://192.168.1.1:80";
+  };
+
+  environment.systemPackages = with pkgs; [
+    docker-compose
+  ];
+
   # Remember to add any user the "docker" group for non-sudo socket access.
   virtualisation.docker = {
     enable = true;
